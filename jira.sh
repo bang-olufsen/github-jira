0ec320b132024b04c8d6abb3ba151ecea69ae72c
Jeppe Frandsen <jeppe@algrens.dk>
2018-06-04 09:23:41 +0200

    Removed output from curl commands

--- jira.sh
+++ jira.sh
@@ -8,7 +8,7 @@ status () {
     if [ "$IS_PULL_REQUEST" = "true" ]; then
       DESCRIPTION=`echo $2 | cut -b -100`
       DATA="{ \"state\": \"$1\", \"target_url\": \"$BUILD_URL\", \"description\": \"$DESCRIPTION\", \"context\": \"jira\"}"
-      curl -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -H "User-Agent: bangolufsen/jira" -X POST -d "$DATA" $GITHUB_API/statuses/$COMMIT 1>/dev/null
+      curl -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -H "User-Agent: bangolufsen/jira" -X POST -d "$DATA" $GITHUB_API/statuses/$COMMIT 1>/dev/null 2>&1
     fi
  fi
 }
@@ -16,7 +16,7 @@ status () {
 jira_number() {
   if [ "$SHIPPABLE" = "true" ]; then
     if [ "$IS_PULL_REQUEST" = "true" ]; then
-      curl -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -H "User-Agent: bangolufsen/jira" -X GET $GITHUB_API/pulls/$PULL_REQUEST | grep -Po '"title":"[A-Z]+-[0-9]+' | cut -f4 -d '"' 1>/dev/null
+      curl -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -H "User-Agent: bangolufsen/jira" -X GET $GITHUB_API/pulls/$PULL_REQUEST | grep -Po '"title":"[A-Z]+-[0-9]+' | cut -f4 -d '"' 1>/dev/null 2>&1
     fi
   fi
 }
